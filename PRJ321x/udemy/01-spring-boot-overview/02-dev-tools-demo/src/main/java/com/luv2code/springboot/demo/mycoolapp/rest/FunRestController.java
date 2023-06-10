f9335c7ea1c162c30bd41be258d98fba24fa4b67
Thanh LE <vnlemanhthanh@gmail.com>
2023-06-11 00:16:35 +0700

    spring boot devtools

--- PRJ321x/udemy/01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ PRJ321x/udemy/01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,5 +1,21 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
-public class FunRestController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class FunRestController {
+    // expose "/" that return "Hello World!"
+
+    @GetMapping("/")
+    public String sayHello() {
+        return  "Hello World!";
+    }
+
+    // expose a new enpoint for "workout"
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
 }

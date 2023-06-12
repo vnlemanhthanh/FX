af4d5e646241c20f829394a9c8949439680066be
Thanh LE <vnlemanhthanh@gmail.com>
2023-06-12 08:09:39 +0700

    02-04 qualifiers

--- PRJ321x/udemy/02-spring-boot-spring-core/04-qualifiers/src/main/java/com/fx/springcoredemo/rest/DemoController.java
+++ PRJ321x/udemy/02-spring-boot-spring-core/04-qualifiers/src/main/java/com/fx/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.fx.springcoredemo.rest;
 
 import com.fx.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public DemoController(Coach theCoach) {
+    public DemoController(@Qualifier("baseballCoach") Coach theCoach) {
         myCoach = theCoach;
     }
 

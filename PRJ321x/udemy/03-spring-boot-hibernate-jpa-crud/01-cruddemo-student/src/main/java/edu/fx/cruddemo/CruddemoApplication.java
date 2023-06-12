ba868899eba243ace97216ed6211e5a33c169957
Thanh LE <vnlemanhthanh@gmail.com>
2023-06-12 21:28:15 +0700

    03-01 cruddemo student

--- PRJ321x/udemy/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/edu/fx/cruddemo/CruddemoApplication.java
+++ PRJ321x/udemy/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/edu/fx/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package edu.fx.cruddemo;
 
+import edu.fx.cruddemo.dao.StudentDAO;
+import edu.fx.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,48 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 
 		return runner -> {
-			System.out.println("Hello World!");
+			createSudent(studentDAO);
+			createMultipleStudents(studentDAO);
 		};
 	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		// create multiple students
+		System.out.println("Creating 3 student objects ...");
+		Student tempStudent1 = new Student("John", "Doe", "john@fx.edu");
+		Student tempStudent2 = new Student("Mary", "Public", "mary@fx.edu");
+		Student tempStudent3 = new Student("Bonita", "Applebum", "bonita@fx.edu");
+
+		// save the student objects
+		System.out.println("Saving the students ...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+	}
+
+	private void createSudent(StudentDAO studentDAO) {
+
+		// create the student object
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Paul", "Doe", "paul@fx.edu");
+
+		// save the stuent object
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
+
+	}
 }
+
+
+
+
+
+
+
+

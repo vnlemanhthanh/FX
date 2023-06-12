cf8d9e90c863654e96f9ea04c83ddefd88eb635b
Thanh LE <vnlemanhthanh@gmail.com>
2023-06-12 09:21:30 +0700

    02-06 lazy initialization

---
+++ PRJ321x/udemy/02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/fx/springcoredemo/rest/DemoController.java
@@ -0,0 +1,26 @@
+package com.fx.springcoredemo.rest;
+
+import com.fx.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+
+    private Coach myCoach;
+
+    @Autowired
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}

668dae805f9f72bfb2c43311eca46c713a310de0
Thanh LE <vnlemanhthanh@gmail.com>
2023-06-18 01:57:56 +0700

    test

--- PRO192x/Lab/src/Lab1/Lab11/Stewie.java
+++ PRO192x/Lab/src/Lab1/Lab11/Stewie.java
@@ -4,6 +4,6 @@ public class Stewie {
     public static void main(String[]agrs) {
         System.out.println("//////////////////////\n|| Victory is mine! ||");
         System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");        
-        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        // System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");  
     }
 }

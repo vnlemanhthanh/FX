0a2ef769c34c4da6a796ba62855b5ae32424d75d
Thanh LE <vnlemanhthanh@gmail.com>
2023-06-21 22:30:44 +0700

    lab 2-4

---
+++ PRJ321x/lab/lab2-4/src/BaseballCoach.java
@@ -0,0 +1,10 @@
+
+
+public class BaseballCoach implements Coach {
+	
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Spend 30 minutes on batting practice";
+	}
+}
---
+++ PRJ321x/lab/lab2-4/src/Coach.java
@@ -0,0 +1,7 @@
+
+
+public interface Coach {
+	
+	public String getDailyWorkout();
+	
+}

b857da4ddc911d9c0f69d34b9af0b0352d1bfe7b
Thanh LE <vnlemanhthanh@gmail.com>
2023-06-28 15:42:55 +0700

    dbi202

--- DBI202x/[70-461]/70-461.sql
+++ DBI202x/[70-461]/70-461.sql
@@ -5,6 +5,8 @@ USE [70-461]
 GO
 
 DROP TABLE tblEmployee
+ 
+SELECT * FROM tblEmployee
 
 CREATE TABLE tblEmployee(
 EmployeeNumber INT NOT NULL,
@@ -15,12 +17,26 @@ EmployeeGovermentID CHAR(10),
 DateOfBirth DATE NOT NULL
 )
 
+ALTER TABLE tblEmployee
+ADD Department VARCHAR(10);
 
-DROP TABLE tblSecond
+INSERT INTO tblEmployee
+VALUES (132, 'Dylan', 'A', 'Word', 'HN513777D', '19920914', 'Customer Relations')
 
-CREATE TABLE tblTransaction (
-Amount SMALLMONEY NOT NULL,
-DateOfTransaction SMALLDATETIME,
-EmployeeNumber INT NOT NULL
-)
+ALTER TABLE tblEmployee
+DROP COLUMN Department
 
+ALTER TABLE tblEmployee
+ADD Department VARCHAR(15)
+
+ALTER TABLE tblEmployee
+ALTER COLUMN Department VARCHAR(20)
+
+ALTER TABLE tblEmployee
+ALTER COLUMN Department VARCHAR(19)
+
+SELECT LEN('Customer Relations')
+
+INSERT INTO tblEmployee([EmployeeFirstName],[EmployeeMiddleName],
+[EmployeeLastName], [EmployeeGovermentID],[DateOfBirth], [Department],[EmployeeNumber])
+VALUES ('Jossef', 'H', 'Wright', 'TX593671R', '19711224', 'Litigation',131);

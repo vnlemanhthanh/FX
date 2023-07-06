874e34dd491386bd7e4ce0a782cf59fa0fcd6931
Thanh LE <vnlemanhthanh@gmail.com>
2023-07-07 00:07:46 +0700

    one-to-one-bi delete-instructor-detail-only

--- PRJ321x/lab/lab8.0.1/src/com/luv2code/hibernate/demo/CreateDemo.java
+++ PRJ321x/lab/lab8.0.1/src/com/luv2code/hibernate/demo/CreateDemo.java
@@ -4,46 +4,60 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
 import com.luv2code.hibernate.demo.entity.Student;
 
-public class CreateStudentDemo {
+public class CreateDemo {
 
 	public static void main(String[] args) {
 
 		// create session factory
-		SessionFactory factory = new Configuration()
-								.configure("hibernate.cfg.xml")
-								.addAnnotatedClass(Student.class)
-								.buildSessionFactory();
-		
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).buildSessionFactory();
+
 		// create session
 		Session session = factory.getCurrentSession();
-		
-		try {			
-			// create a student object
-			System.out.println("Creating new student object...");
-			Student tempStudent = new Student("Paul", "Doe", "paul@luv2code.com");
+
+		try {
+			// create the objects
+
+			/*
+			Instructor tempInstructor = new Instructor("Chad", "Darby", "darby@luv2code.com");
+
+			InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.luv2code.com/youtube",
+					"Luv 2 code!!!");
+			*/
 			
+			Instructor tempInstructor = 
+					new Instructor("Madhu", "Patel", "madhu@luv2code.com");
+			
+			InstructorDetail tempInstructorDetail =
+					new InstructorDetail(
+							"http://www.youtube.com",
+							"Guitar");	
+			
+			// associate the objects
+			tempInstructor.setInstructorDetail(tempInstructorDetail);
+
 			// start a transaction
 			session.beginTransaction();
-			
-			// save the student object
-			System.out.println("Saving the student...");
-			session.save(tempStudent);
-			
+
+			// save the instructor
+			//
+			// Note: this will ALSO save the details object
+			// because of CascadeType.ALL
+			//
+			System.out.println("Saving instructor: " + tempInstructor);
+			session.save(tempInstructor);
+
 			// commit transaction
 			session.getTransaction().commit();
-			
+
 			System.out.println("Done!");
-		}
-		finally {
+		} finally {
 			factory.close();
 		}
 	}
 
 }
-
-
-
-
-

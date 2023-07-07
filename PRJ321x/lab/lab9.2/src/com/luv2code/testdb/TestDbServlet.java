5d190f1457b2697d7be8d2e21573692fb20a2532
Thanh LE <vnlemanhthanh@gmail.com>
2023-07-07 22:36:56 +0700

    Hibernate Entity

--- PRJ321x/lab/lab9.2/src/com/luv2code/testdb/TestDbServlet.java
+++ PRJ321x/lab/lab9.2/src/com/luv2code/testdb/TestDbServlet.java
@@ -1,6 +1,10 @@
 package com.luv2code.testdb;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,11 +19,38 @@ public class TestDbServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		// setup connection variables
+		String user = "springstudent";
+		String pass = "springstudent";
+
+		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC";
+		String driver = "com.mysql.jdbc.Driver";
+
+		// get connection to database
+		try {
+			PrintWriter out = response.getWriter();
+
+			out.println("Connecting to database: " + jdbcUrl);
+
+			Class.forName(driver);
+
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+
+			out.println("\nSUCCESS!!!");
+
+			myConn.close();
+
+		} catch (Exception exc) {
+			exc.printStackTrace();
+			throw new ServletException(exc);
+		}
+
 	}
 
 }
